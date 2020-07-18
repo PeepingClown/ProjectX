fd0c6ed51a025108c94b47a8e787d87f63b6b771
Mayanka Chattopadhyay <jodu892@gmail.com>
2020-07-18 17:06:50 +0530

    added login page

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ const http = require('http');
 
 const app = express();
 
+
+app.use(express.static('public'));
 app.set('view engine', "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req,res)=>{
+
   res.render("home");
 })
 
-app.use(express.static('public'))
+app.get("/login", (req,res)=>{
+  res.sendFile(__dirname+"/index.html");
+})
+app.post("/signup", (req,res) => {
+  res.redirect("/login");
+})
+
+
 
 app.listen(3000,()=>{
   console.log("Running on port:3000");
